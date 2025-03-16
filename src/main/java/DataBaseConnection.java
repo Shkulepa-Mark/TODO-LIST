4659dab65c02d64aef6e3e2a16e1242d55a4374e
shkulepa mark <skulepamark@gmail.com>
2025-03-16 20:31:38 +0300

    Added class for connection to database

--- src/main/java/DataBaseConnection.java
+++ src/main/java/DataBaseConnection.java
@@ -1,2 +1,43 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DataBaseConnection {
+    public static Connection getConnection() {
+        String user = null;
+        String password = null;
+        String url = null;
+
+        Properties prop = new Properties();
+        InputStream fis;
+
+        try {
+            fis = DataBaseConnection.class.getClassLoader().getResourceAsStream("config.properties");
+
+            prop.load(fis);
+
+            user = prop.getProperty("DBUser");
+            password = prop.getProperty("DBPass");
+            url = prop.getProperty("DBUrl");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Connection connect = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            connect = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return connect;
+    }
 }
