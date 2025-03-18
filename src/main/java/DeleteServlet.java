c6dfb2e9338e15ea0c5cd7a0c6be5cd97a13a2bc
shkulepa mark <skulepamark@gmail.com>
2025-03-18 21:20:37 +0300

    Implemented servlet for deleting the task from list

---
+++ src/main/java/DeleteServlet.java
@@ -0,0 +1,20 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/delTask")
+public class DeleteServlet extends HttpServlet {
+    private TaskDAO taskDAO = new TaskDAO();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int taskId = Integer.parseInt(req.getParameter("id"));
+
+        taskDAO.deleteTask(taskId);
+        resp.sendRedirect("displayTasks");
+    }
+}
