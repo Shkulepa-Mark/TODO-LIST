43f3e1dd1a542d58bc746f3b6f6c29b72e2a1a9d
shkulepa mark <skulepamark@gmail.com>
2025-03-18 19:52:29 +0300

    Added method for get task by id

--- src/main/java/TaskDAO.java
+++ src/main/java/TaskDAO.java
@@ -1,3 +1,4 @@
+import javax.xml.crypto.Data;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,30 @@ public class TaskDAO {
         }
 
         return tasks;
+    }
+
+    public Task getTaskById(int taskId) {
+        String query = "SELECT * FROM Tasks WHERE id = ?";
+        Task task = null;
+
+        try (Connection connection = DataBaseConnection.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, taskId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                task = new Task(
+                resultSet.getInt("id"),
+                resultSet.getString("title"),
+                resultSet.getString("description"),
+                resultSet.getBoolean("status")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return task;
     }
 
     public void addTask(Task task) {
