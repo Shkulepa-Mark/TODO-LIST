1a87aa00e2fc9b32f7b170ffcae6acd1412370fd
shkulepa mark <skulepamark@gmail.com>
2025-03-18 21:21:14 +0300

    Implemented servlet for displaying all tasks

---
+++ src/main/java/TaskServlet.java
@@ -0,0 +1,25 @@
+import com.example.todo.Task;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/displayTasks")
+public class TaskServlet extends HttpServlet {
+    private TaskDAO taskDAO = new TaskDAO();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            List<Task> tasks = taskDAO.getAllTasks();
+            req.setAttribute("tasks", tasks);
+            req.getRequestDispatcher("task.jsp").forward(req, resp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
